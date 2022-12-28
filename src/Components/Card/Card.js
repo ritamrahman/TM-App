f2f572bddead1caa48f3cbd04d3340fa0ddc9168
ritamrahman <ritamrahman@gmail.com>
2022-12-29 01:00:09 +0600

    added my tasks api

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,33 +1,35 @@
+import { async } from "@firebase/util";
+import axios from "axios";
 import React from "react";
+import { toast } from "react-hot-toast";
+import { api } from "../../api/api";
 
-const Card = () => {
+const Card = ({ id, title, image, isCompleted, deleteHandler }) => {
   return (
     <div
       className="relative block rounded-xl border border-yellowGreen p-8 shadow-xl"
       href=""
     >
-      {/* <p className="absolute right-4 top-4 rounded-full bg-purple-200 dark:bg-yellowGreen px-3 py-1.5 text-xs font-medium text-primary">
-        Completed
-      </p> */}
       <div className=" flex justify-end">
-        {/* complete */}
-        <span className="rounded-full bg-purple-200 dark:bg-yellowGreen px-3 py-1.5 text-xs font-medium text-primary mx-2">
-          Completed
-        </span>
-
-        <span className="rounded-full bg-redColor px-3 py-1.5 text-xs font-medium text-primary mx-2">
-          Not Completed
-        </span>
+        {isCompleted ? (
+          <span className="rounded-full bg-purple-200 dark:bg-yellowGreen px-3 py-1.5 text-xs font-medium text-primary mx-2">
+            Completed
+          </span>
+        ) : (
+          <span className="rounded-full bg-redColor px-3 py-1.5 text-xs font-medium text-primary mx-2">
+            Not Completed
+          </span>
+        )}
       </div>
       <div className="mt-4 text-gray-500 dark:text-primary sm:pr-8">
         <img
-          src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+          src={`${image}?${image}:"https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"`}
           alt=""
           className="max-h-[100px] rounded-2xl"
         />
 
         <h3 className="mt-4 text-xl font-bold text-matBlack-900 dark:text-primary">
-          Science of Chemistry
+          {title}
         </h3>
 
         <p className="mt-2 hidden text-sm sm:block">
@@ -69,20 +71,22 @@ const Card = () => {
         </svg>
 
         {/* delete */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 text-matBlack-900 dark:text-primary cursor-pointer mx-1"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-          />
-        </svg>
+        <button onClick={() => deleteHandler(id)}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 text-matBlack-900 dark:text-primary cursor-pointer mx-1"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
--- src/pages/MyTasks.js
+++ src/pages/MyTasks.js
@@ -1,12 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { api } from "../api/api";
 import Card from "../Components/Card/Card";
+import { AuthContext } from "../contexts/AuthProvider";
+import Loading from "../Components/Loading/Loading";
+import NotFound from "../Components/Error/NotFound";
 
 const MyTasks = () => {
-  return (
-    <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 mx-5 py-10 gap-3">
-      <Card />
-      <Card />
-    </div>
+  const { user, loading } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [allTasks, setAllTasks] = useState();
+  const [taskApiCall, setTaskApiCall] = useState(false);
+
+  // call all tasks api
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(`${api}/tasks/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAllTasks(data);
+        setIsLoading(false);
+      })
+      .catch((er) => console.error(er));
+  }, [user?.email, taskApiCall]);
+
+  console.log(allTasks);
+
+  // deleteHandler
+  const deleteHandler = async (id) => {
+    const result = await axios.delete(`${api}/task/${id}`);
+    result.success === true && toast.success("Task deleted successfully");
+    setTaskApiCall(true);
+  };
+
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <>
+      {allTasks?.tasks?.length === 0 ? (
+        <NotFound />
+      ) : (
+        <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 mx-5 py-10 gap-3">
+          {allTasks?.tasks.map((task) => (
+            <Card
+              id={task._id}
+              title={task.title}
+              image={task.image}
+              isCompleted={task.isCompleted}
+              deleteHandler={deleteHandler}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
